755e506e3596670ea945e3fe9c14ddea7e524da2
mayank09051993 <mayankagar.09@gmail.com>
2019-07-11 11:26:16 +0530

    Added Vibrator & Notification Service When Sensitive Information Id Detected.

--- app/src/main/java/com/example/b_aware/IListeningService2.java
+++ app/src/main/java/com/example/b_aware/IListeningService2.java
@@ -1,19 +1,20 @@
 package com.example.b_aware;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
-import android.telephony.TelephonyManager;
 import android.util.Log;
-
-import com.google.android.material.tabs.TabLayout;
-
+import android.view.View;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -23,6 +24,7 @@ public class IListeningService2 extends Service {
     boolean isListening = false;
 
     public IListeningService2() {
+
         mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
@@ -69,12 +71,26 @@ public class IListeningService2 extends Service {
                 //displaying the first match
                 if (matches != null)
                     Log.i("TAG",matches.get(0));
+
+                mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
             }
 
             @Override
             public void onPartialResults(Bundle bundle) {
                 ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                 ArrayList<String> unstableData = bundle.getStringArrayList("android.speech.extra.UNSTABLE_TEXT");
+                if((data.get(0) + unstableData.get(0)).toUpperCase().contains("CARD") ||
+                        (data.get(0) + unstableData.get(0)).toUpperCase().contains("OTP") ||
+                        (data.get(0) + unstableData.get(0)).toUpperCase().contains("CVV"))
+                {
+                    Log.i("Sensitive Information", "Sensitive Information Asked...!!!");
+                    createNotification(null);
+                    Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                    if (Build.VERSION.SDK_INT >= 26) {
+                        vibrator.vibrate(VibrationEffect.createOneShot(2000, VibrationEffect.EFFECT_HEAVY_CLICK));
+                    }
+                }
+
                 Log.i("partial", data.get(0) + unstableData.get(0));
             }
 
@@ -101,8 +117,8 @@ public class IListeningService2 extends Service {
             Log.i("TAG", "Started Listening");
         }
         else if(isListening && (System.nanoTime() - startTime) > Long.parseLong("1000000000")){
-            mSpeechRecognizer.stopListening();
             Log.i("TAG", "Stopped Service");
+            mSpeechRecognizer.stopListening();
         }
         return  START_STICKY;
     }
@@ -110,5 +126,20 @@ public class IListeningService2 extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+
+    public void createNotification(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Fraud Call Alert.")
+                .setContentText("Sensitive Information Asked.").setSmallIcon(R.drawable.baware_icon)
+                .setContentIntent(pIntent)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(0, notification);
+
     }
 }
